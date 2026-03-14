e844ba704a7205896a7f736dc3933e3e322ad647
NajoroRabiaza <hei.najoro.2@gmail.com>
2026-03-14 15:30:37 +0300

    feat: ajout des pages resultats, scenarios et historique

--- interface/urls.py
+++ interface/urls.py
@@ -5,4 +5,7 @@ app_name = 'interface'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('resultats/', views.resultats, name='resultats'),
+    path('scenarios/', views.scenarios, name='scenarios'),
+    path('historique/', views.historique, name='historique'),
 ]
--- interface/views.py
+++ interface/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'interface/index.html')
+
+
+def resultats(request):
+    return render(request, 'interface/resultats.html')
+
+
+def scenarios(request):
+    return render(request, 'interface/scenarios.html')
+
+
+def historique(request):
+    return render(request, 'interface/historique.html')
